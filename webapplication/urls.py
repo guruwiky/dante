2a9d9b4415fcc60aa3e9385fd5edaf0af4ac8a12
guruwiky <ochiengwyclife6@gmail.com>
2022-11-11 22:34:49 +0300

    added requirement file and updated url file
    changed needed to heroku deployement

--- webapplication/urls.py
+++ webapplication/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import include,re_path
 
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('',include('accounts.urls')),
+	re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 	
 ]
 	
