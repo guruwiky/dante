372e1c1e2047fcaaf3a318e56e004a245e9bd079
guruwiky <ochiengwyclife6@gmail.com>
2022-11-12 17:41:34 +0300

    removed module heroku

--- webapplication/settings.py
+++ webapplication/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     "phonenumber_field",
-    "django_heroku",
+    
     
 ]
 
@@ -131,4 +131,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+
